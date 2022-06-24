90dc6be86fa5be3428b711ecc252f3ad3f4c269f
Franco Ortega <francopdx@gmail.com>
2022-06-23 21:09:16 -0700

    install Next

---
+++ README.md
@@ -0,0 +1,3 @@
+### Dependencies
+1. npx create-next-app@latest
+1. npm install --save-dev sass
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+// import Image from 'next/image';
+// giimport styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Portfolio Playground</title>
+        <meta name="description" content="Playground for experimenting with portfolio ideas." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>Header?</header>
+      <main>Welcome to the Playground.</main>
+      <footer>Footer!</footer>
+    </div>
+  );
+}
