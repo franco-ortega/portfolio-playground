bb75b520930e8ee0e4e8b1cf8f662e79de78b615
Franco Ortega <francopdx@gmail.com>
2022-06-27 19:14:58 -0700

    make sidebar open and close on hover

--- README.md
+++ README.md
@@ -11,3 +11,10 @@
 1. npm i --save @fortawesome/free-regular-svg-icons
 1. npm i --save @fortawesome/react-fontawesome@latest
 1. https://fontawesome.com/v6/docs/web/use-with/react/use-with#troubleshooting-with-next-js
+
+### Current Issues
+1. How to best handle hamburger menu on mobile?
+    1. Right now the the sidebar on mobile opens when it is clicked.
+    1. The sidebar on mobile closes when the area outside the sidebar is clicked.
+    1. After the sidebar is open, the hamburger menu seems to work correctly. However, on page load the sidebar is closed, and clicking on the hamburger menu first triggers the onMouseOver to make the sidebarActive true, but then the onHamburgerClick function sets the state to its opposite state, so false, and therefore, the sidebar doesn't open. This behavior also happens anytime that the sidebar is closed by clicking outside the sidebar; then when you click on the hamburger, nothing seems to happen.
+1. Make sure the hamburger menu is accessible.
--- components/sidebar/Sidebar.jsx
+++ components/sidebar/Sidebar.jsx
@@ -4,31 +4,77 @@ import {
   faUser,
   faFolder,
   faFile,
+  faHashtag,
+  faBars,
 } from '@fortawesome/free-solid-svg-icons';
 import styles from './Sidebar.module.scss';
 import { faAddressBook } from '@fortawesome/free-regular-svg-icons';
+import { useState } from 'react';
 
 const Sidebar = () => {
+  const [sidebarActive, setSidebarActive] = useState(false);
+
+  const onMouseOverSidebar = () => {
+    console.log('on mouse over');
+    setSidebarActive(true);
+  };
+
+  const onMouseOutsideSidebar = () => {
+    console.log('on mouse outside');
+    setSidebarActive(false);
+  };
+
+  console.log(sidebarActive);
+
+  const onHamburgerClick = () => {
+    console.log('HANBURGER!!!');
+    setSidebarActive((prevState) => !prevState);
+  };
+
   return (
-    <nav className={styles.Sidebar}>
+    <nav
+      className={
+        sidebarActive
+          ? styles.Sidebar
+          : `${styles.Sidebar} ${styles.SidebarClosed}`
+      }
+      // onMouseOver={onMouseOverSidebar}
+      // onMouseOut={onMouseOutsideSidebar}
+    >
+      <p
+        className={styles.Hamburger}
+        onClick={onHamburgerClick}
+        // onMouseOver={onMouseOverSidebar}
+        // onMouseOut={onMouseOutsideSidebar}
+      >
+        <FontAwesomeIcon icon={faBars} />
+      </p>
       <ul>
         <li>
-          <FontAwesomeIcon icon={faHouse} /> Home
+          <FontAwesomeIcon icon={faHouse} /> <span>Home</span>
         </li>
         <li>
-          <FontAwesomeIcon icon={faUser} /> About
+          <FontAwesomeIcon icon={faUser} /> <span>About</span>
         </li>
         <li>
-          <FontAwesomeIcon icon={faFolder} />
-          Projects
+          <FontAwesomeIcon icon={faFolder} /> <span>Projects</span>
         </li>
         <li>
-          <FontAwesomeIcon icon={faAddressBook} />
-          Contact
+          <FontAwesomeIcon icon={faAddressBook} /> <span>Contact</span>
         </li>
         <li>
-          <FontAwesomeIcon icon={faFile} />
-          Resume
+          <FontAwesomeIcon icon={faFile} /> <span>Resume</span>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <FontAwesomeIcon icon={faHashtag} /> <span>GitHub</span>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faHashtag} /> <span>LinkedIn</span>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faHashtag} /> <span>Twitter</span>
         </li>
       </ul>
     </nav>
