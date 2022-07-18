2d03843df6cb8d56cfa6d29a1eb2dcae862fd9a0
Franco Ortega <francopdx@gmail.com>
2022-07-18 16:22:05 -0700

    replace side nav with top nav

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
-import Sidebar from '../sidebar/Sidebar';
-// import Nav from '../nav/Nav';
+import Nav from '../nav/Nav';
 import styles from './Layout.module.scss';
 
 const Layout = ({ children }) => {
@@ -15,11 +14,9 @@ const Layout = ({ children }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header>
-        {/* <Nav /> */}
-        <Sidebar />
+        <Nav />
       </header>
       <main>{children}</main>
-      {/* <footer>Footer!</footer> */}
     </div>
   );
 };
