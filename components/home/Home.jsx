e6a2753ec2c272843dc8ed0ae928fe8c691816f6
Franco Ortega <francopdx@gmail.com>
2022-07-18 19:13:39 -0700

    add initial layout to Home

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -5,20 +5,24 @@ import styles from './Home.module.scss';
 const Home = () => {
   return (
     <div className={styles.Home}>
-      <p>
-        Welcome! My name is <span>Franco Ortega</span>.
-      </p>
-      <Image src={profilePic} alt="" width="1160" height="1160"></Image>
-      <p>
-        I&apos;m a Full Stack Software Engineer who leans toward the frontend. I
-        love to create intuitive interfaces paired with proper guidance. I
-        appreciate well-documented and efficient processes that offer both
-        structure and flexibility. I value clear and kind communication,
-        creativity, cross-functional collaboration, and supportive environments
-        that foster diversity and nurture growth. Optimism guides my efforts
-        while practicality ensures that my goals are brought to fruition.
-        Helping others brings me great joy.
-      </p>
+      <section>
+        <p>
+          Welcome! My name is <span>Franco Ortega</span>.
+        </p>
+        <Image src={profilePic} alt="" width="1160" height="1160"></Image>
+      </section>
+      <section>
+        <p>
+          I&apos;m a Full Stack Software Engineer who leans into the frontend.
+          I love to create intuitive interfaces paired with proper guidance. I
+          appreciate well-documented and efficient processes that offer both
+          structure and flexibility. I value clear and kind communication,
+          creativity, cross-functional collaboration, and supportive
+          environments that foster diversity and nurture growth. Optimism guides
+          my efforts while practicality ensures that my goals are brought to
+          fruition. Helping others brings me great joy.
+        </p>
+      </section>
     </div>
   );
 };
