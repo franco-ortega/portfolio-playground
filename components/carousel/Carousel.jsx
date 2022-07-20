1cd4a8cf27dce824d875aaab6ea7b2e3c6de33ac
Franco Ortega <francopdx@gmail.com>
2022-07-19 17:18:55 -0700

    handle swipe event

--- components/carousel/Carousel.jsx
+++ components/carousel/Carousel.jsx
@@ -4,6 +4,7 @@ import styles from './Carousel.module.scss';
 const Carousel = ({ children }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [length, setLength] = useState(children.length);
+  const [touchPosition, setTouchPosition] = useState(null);
 
   useEffect(() => {
     setLength(children.length);
@@ -21,13 +22,32 @@ const Carousel = ({ children }) => {
     }
   };
 
+  const handleTouchStart = (e) => {
+    const touchDown = e.touches[0].clientX;
+    setTouchPosition(touchDown);
+  };
+
+  const handleTouchMove = (e) => {
+    const touchDown = touchPosition;
+
+    if (touchDown === null) return;
+
+    const currentTouch = e.touches[0].clientX;
+    const diff = touchDown - currentTouch;
+
+    if (diff > 5) onNextClick();
+    if (diff < -5) onPrevClick();
+
+    setTouchPosition(null);
+  };
+
   return (
     <div className={styles.Carousel}>
       <div>
         <button onClick={onPrevClick} disabled={!currentIndex}>
           &lt;
         </button>
-        <div>
+        <div onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}>
           <div style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
             {children}
           </div>
