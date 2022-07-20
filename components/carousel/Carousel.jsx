2edbbf93992e7a61d6ac4a62795bf1531e268e6d
Franco Ortega <francopdx@gmail.com>
2022-07-19 17:12:54 -0700

    remove console logs

--- components/carousel/Carousel.jsx
+++ components/carousel/Carousel.jsx
@@ -2,11 +2,8 @@ import { useEffect, useState } from 'react';
 import styles from './Carousel.module.scss';
 
 const Carousel = ({ children }) => {
-  console.log('CHILDREN LENGTH: ', children.length);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [length, setLength] = useState(children.length);
-  console.log('Current Index: ', currentIndex);
-  console.log('LENGTH: ', length);
 
   useEffect(() => {
     setLength(children.length);
@@ -27,13 +24,17 @@ const Carousel = ({ children }) => {
   return (
     <div className={styles.Carousel}>
       <div>
-        <button onClick={onPrevClick} disabled={!currentIndex}>&lt;</button>
+        <button onClick={onPrevClick} disabled={!currentIndex}>
+          &lt;
+        </button>
         <div>
           <div style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
             {children}
           </div>
         </div>
-        <button onClick={onNextClick} disabled={currentIndex === length - 1}>&gt;</button>
+        <button onClick={onNextClick} disabled={currentIndex === length - 1}>
+          &gt;
+        </button>
       </div>
     </div>
   );
